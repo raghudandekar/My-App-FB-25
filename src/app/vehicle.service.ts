1a9c72da7ef8b5c220bb78ba4150fdb74cc2a103
mnshireesha1 <mnshireesha@gmail.com>
2025-03-19 15:15:42 +0530

    some api possible calls are done

--- src/app/vehicle.service.ts
+++ src/app/vehicle.service.ts
@@ -12,4 +12,19 @@ export class VehicleService {
     return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction")
   }
 
+  getFilteredVechicles(term:any):Observable<any>{
+    return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?filter="+term);
+  }
+
+  getSortVehicles(column:any,order:any):Observable<any>{
+    return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?sortBy="+column+"&order="+order);
+  }
+
+deleteVehicle(id:any):Observable<any>{
+  return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction/"+id);
+}
+getpaginationVehicles(limit:any,page:any){
+  return this._httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?limit="+limit+"&page="+page);
+}
+
 }
--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -7,14 +7,67 @@ import { VehicleService } from '../vehicle.service';
   styleUrls: ['./vehicle.component.css']
 })
 export class VehicleComponent {
+  term:string='';
   vehicles:any=[];
 constructor(private _VehicleService:VehicleService){
-  _VehicleService.getVehicles().subscribe((data:any)=>{
+  this.loadVehicles();
+}
+loadVehicles(){
+  this._VehicleService.getVehicles().subscribe((data:any)=>{
     console.log(data);
     this.vehicles=data;
   },(error:any)=>{
-    alert("Internal Server Error");
+    alert("Internal Server Error!");
   }
 )
 }
+
+filter(){
+  this._VehicleService.getFilteredVechicles(this.term).subscribe((data:any)=>{
+    console.log(data);
+    this.vehicles=data;
+  },(err:any)=>{
+    alert("Internal Server Error!");
+  })
 }
+column:string='';
+order:string='';
+
+sort(){
+  this._VehicleService.getSortVehicles(this.column,this.order).subscribe((data:any)=>{
+    console.log(data);
+    this.vehicles=data;
+  },(err:any)=>{
+    alert("Internal Sefver Error!");
+
+  })
+}
+
+delete(id:any){
+  if(confirm("Are you sure to delete?")==true){
+    this._VehicleService.deleteVehicle(id).subscribe((data:any)=>{
+      alert("Record Deleted Successfully!")
+      this.loadVehicles();
+    },(err:any)=>{
+      alert("Internal Server Error!");
+    })
+    
+  }else{
+    alert("You Have cancelled");
+  
+}
+}
+
+limit:string='';
+page:string='';
+pagination(){
+  this._VehicleService.getpaginationVehicles(this.limit,this.page).subscribe((data:any)=>{
+    console.log(data);
+    this.vehicles=data;
+  },(err:any)=>{
+    alert("Internal Server Error!")
+  })
+  }
+}
+
+
